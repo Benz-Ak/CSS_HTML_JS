47a28ac96e16ead62f61dadba1e82d0fce04ca2a
Benz-Ak <akoayabenz8@gmail.com>
2025-04-22 13:00:09 +0100

    first commit

---
+++ Basic_HTML_Webite/asset/js/script.js
@@ -0,0 +1,11 @@
+/* function presentation(nom, prenom, age) {
+  alert(
+    "Bonjour je m'appelle " + prenom + " " + nom + " et j'ai " + age + " ans."
+  );
+}
+let nom, prenom, age;
+nom = prompt("Veuillez entrer votre nom :");
+prenom = prompt("Veuillez entrer votre prénom :");
+age = prompt("Veuillez entrer votre âge :");
+presentation(nom, prenom, age);
+ */
